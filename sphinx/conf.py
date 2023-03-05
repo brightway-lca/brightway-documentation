a833d8ff971696ca54d98bfe52dd7d996c373ab7
Michael <23102087+michaelweinold@users.noreply.github.com>
2023-03-05 09:19:17 +0100

    fixed static path

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -163,7 +163,7 @@ html_theme_options = {
     # https://pydata-sphinx-theme.readthedocs.io/en/stable/user_guide/version-dropdown.html
     "switcher": {
         "version_match": version,
-        "json_url": "https://raw.githubusercontent.com/brightway-lca/brightway-documentation/main/sphinx/source/_static/switcher.json",
+        "json_url": "https://documentation.brightway.dev/en/latest/_static/switcher.json",
     },
     "navbar_start": ["navbar-logo", "version-switcher"],
     "header_links_before_dropdown": 7,
