1325b225e45f3f325431f5aae4e300d21936d9cb
Michael <23102087+michaelweinold@users.noreply.github.com>
2023-03-19 10:59:46 +0100

    fixed url

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -167,7 +167,7 @@ html_theme_options = {
     },
     "navbar_start": ["navbar-logo", "version-switcher"],
     "header_links_before_dropdown": 7,
-    "announcement": "<p>Register Now: <a href='http://example.com'>Autumn School 2023: Open Approaches to Time in LCA</a> 📍 Switzerland 🗓️ 9-13.October</p>",
+    "announcement": "<p>Register Now: <a href='https://www.d-d-s.ch/school.html'>Autumn School 2023: Open Approaches to Time in LCA</a> 📍 Switzerland 🗓️ 9-13.October</p>",
     "collapse_navigation": True,
     "footer_start": ["copyright"],
     "external_links": [
